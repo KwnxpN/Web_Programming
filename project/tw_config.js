d11856ce2a635d79b8595f343ba87652ae429430
KwnxpN <tonnum.kawin@gmail.com>
2024-12-24 22:44:57 +0700

    commit project progress

---
+++ project/tw_config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme: {
+      
+        extend: {
+          fontFamily: {
+            'pacifico': ["Pacifico", "serif"]
+          },
+          colors: {
+            primary: '#FF7700'
+          }
+        },
+      },
+}
