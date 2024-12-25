e141838abefe3131e404077b1391c9e2c24c0e4e
Babe <tonnum.kawin@gmail.com>
2024-12-25 15:34:42 +0700

    a little add when break

--- project/app.js
+++ project/app.js
@@ -3,17 +3,24 @@ document.addEventListener('DOMContentLoaded', function() {
         ['https://images.unsplash.com/photo-1551504734-5ee1c4a1479b', 'Tacos'],
         ['https://images.unsplash.com/photo-1555507036-ab1f4038808a', 'Croissant'],
         ['https://img.freepik.com/free-photo/pork-charcoal-grill_1339-4999.jpg?t=st=1733456264~exp=1733459864~hmac=a3270cd016f1ff0192ba202fafd5a378c6ebf93141c726e0b9d488eae21fff27&w=996', 'Yakiniku'],
-        ['https://images.unsplash.com/photo-1513104890138-7c749659a591', 'Pizza'],
-        ['https://images.unsplash.com/photo-1568901346375-23c9450c58cd', 'Burger'], 
-        ['https://images.unsplash.com/photo-1599232288126-7dbd2127db14?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dG9ydGlsbGF8ZW58MHx8MHx8fDA%3D', 'Tortilla']
+        // ['https://images.unsplash.com/photo-1513104890138-7c749659a591', 'Pizza'],
+        // ['https://images.unsplash.com/photo-1568901346375-23c9450c58cd', 'Burger'], 
+        // ['https://images.unsplash.com/photo-1599232288126-7dbd2127db14?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dG9ydGlsbGF8ZW58MHx8MHx8fDA%3D', 'Tortilla']
         ];
 
     const menuContainer = document.getElementById('menu-container');
     menuItems.forEach(([image, name]) => {
         menuContainer.innerHTML += `
-            <div class="w-44 h-44 rounded overflow-hidden shadow-lg bg-white flex-shrink-0">
-                <p class="p-2">${name}</p>
-                <img src="${image}" alt="${name}" class="w-full h-32 object-cover">
+            <div class="rounded overflow-hidden shadow-lg bg-red-600">
+                <div class="mx-14 mt-4 mb-0">
+                    <img class="w-full h-64 rounded" src="${image}" alt="Sunset in the mountains">
+                </div>
+                <div class="px-6 py-4">
+                    <div class="font-bold text-xl mb-2">${name}</div>
+                    <p class="text-gray-700 text-base">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                    </p>
+                </div>
             </div>
         `;
     });
